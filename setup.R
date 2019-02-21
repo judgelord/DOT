57844a13b8b1cf4b1e67c85e57bd522163e6bed6
judgelord <devin.jl@gmail.com>
2019-02-21 10:13:32 -0600

    find IFRs and unknown end dates

--- setup.R
+++ setup.R
@@ -30,7 +30,8 @@ install.packages(c(requires[to_install], "NA"), repos = "https://cloud.r-project
 
 library(tidyverse)
 library(dplyr) # in case tydyverse fails (problem on linux)
-library(ggplot2)
+library(readr)
+library(ggplot2); theme_set(theme_bw())
 library(magrittr)
 library(XML)
 library(stringr)
@@ -50,6 +51,13 @@ library(knitr)
 library(survminer)
 library(ggfortify)
 library(broom)
+
+knitr::opts_chunk$set(echo = TRUE, 
+                      cache = F, 
+                      fig.width=8.5, 
+                      fig.align = 'center',
+                      fig.path='Figs/',
+                      warning=FALSE, message=FALSE)
 
 # function to fill NAs
 CopyIfNA <- function(x, na.rm = FALSE, ...) na.locf(x, na.rm = na.rm, ...)
